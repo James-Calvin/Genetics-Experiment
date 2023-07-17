480ed21e9c753d2a8baf3bf706d3db90c1963a9b
FadenK <JamesCalvinMeadersIII@gmail.com>
2023-07-17 18:23:46 -0400

    Forming Genetic Objects

--- main.cs
+++ main.cs
@@ -3,43 +3,109 @@ using System;
 /*
 What should the genetics object do?
 Constructor based on parents
-Add traits and which is dominate
+Add traits and which is dominant
 Add genes
 create a default
 create a random
 create a child
 mutate
-allow for co-dominate traits?
+allow for co-dominant traits?
 */
 
-class Trait
+class DnaTemplate
 {
-  private String name;
-  private Int64 minValue;
-  private Int64 maxValue;
 
-  public Trait(String name, Int64 minValue, Int64 maxValue)
+
+  public void AddGeneDefinition(GeneDefinition gene)
   {
-    this.minValue = minValue;
-    this.maxValue = maxValue;
+    throw new NotImplementedException();
+  }
+
+  public Genotype InstantiateGenotype()
+  {
+    throw new NotImplementedException();
   }
 }
 
-class GeneticObject
+class Genotype
 {
 
-  public List<Trait> traits = new List<Trait>();
-
-  public GeneticObject() { }
+}
 
 
+class GeneDefinition
+{
+  string name;
+  int range;
+
+  public GeneDefinition(string name, int range)
+  {
+    this.name = name;
+    this.range = range;
+  }
+
+  public Gene MakeGene()
+  {
+    return new Gene(name, range);
+  }
+
+  public Gene MakeGene(int value1, int value2)
+  {
+    if (!WithinRange(value1)) throw new ArgumentException("value1 out of range");
+    if (!WithinRange(value2)) throw new ArgumentException("value2 out of range");
+    return new Gene(name, range, new int[] { value1, value2 });
+  }
+
+  public bool WithinRange(int value)
+  {
+    return (value < range - 1 && value >= 0);
+  }
 
 }
+
+class Gene
+{
+  string name;
+  int range;
+  int[] values = new int[] { 0, 0 };
+
+  public Gene(string name, int range)
+  {
+    this.name = name;
+    this.range = range;
+  }
+
+  public Gene(string name, int range, int[] values)
+  {
+    this.name = name;
+    this.range = range;
+    this.values = values;
+  }
+
+  public void SetValues(int value1, int value2)
+  {
+    this.values[0] = value1;
+    this.values[1] = value2;
+  }
+}
+
 
 class Program
 {
   public static void Main(string[] args)
   {
-    Console.WriteLine("Hello World");
+    // How would we like to use Genetics?
+
+    // Instantiate a blank template
+    var flowerDna = new DnaTemplate();
+
+    // Define the possible Gene space
+    var shapeGene = new GeneDefinition("shape", 3); // ⚪🤍⬜
+    flowerDna.AddGeneDefinition(shapeGene);
+
+    var colorGene = new GeneDefinition("color", 8); // 💔🧡💛💚💙💜🖤🤍
+    flowerDna.AddGeneDefinition(colorGene);
+
+    var flower1 = flowerDna.InstantiateGenotype();
   }
 }
